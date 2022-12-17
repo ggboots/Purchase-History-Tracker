ccba18ef5a8720a03c7c1605febd347499599f8a
George Boots <ggboots.99@gmail.com>
2022-12-17 13:25:30 +1100

    tinkering, moving repo

--- pages/api/loginAPI.js
+++ pages/api/loginAPI.js
@@ -1,19 +1,24 @@
 import connectMongo from "../../backend/importConnectMongodb";
 import loginSchema from "../../backend/loginSchema";
-// import fs from 'fs/promises'
-// fs module not working, jsonfile works
 const jsonfile = require('jsonfile')
+const cookieParser = require("cookie-parser");
 
 connectMongo();
+cookieParser();
 
 export default async function handler(req,res){
     const {username, password} = req.body
     const login = await loginSchema.findOne({username,password})
+    // res.setHeader('Custom-Header', username)
+
+    req.cookies
     if(!login){
         return res.json({status: "incorrect password"})
     }
     else{
         jsonfile.writeFile('./backend/fromDatabase.json', login)
+        res.setHeader('Set-Cookie',username)
+        // document.localStorage.setItem(username, password)
         .then(res => {
             console.log('write complete')
         })
@@ -23,4 +28,8 @@ export default async function handler(req,res){
     }
 }
 
-//https://heynode.com/tutorial/readwrite-json-files-nodejs/
+//cookies vs localstorage
+// https://www.youtube.com/watch?v=AwicscsvGLg&t=338s&ab_channel=freeCodeCamp.org
+
+// API - Acces data, hides abstraction (camera component, barometer), 
+// also for security and 
